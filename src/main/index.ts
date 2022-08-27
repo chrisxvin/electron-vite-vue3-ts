5fa5ce52cacf49f81b0ac2b79fde2e369e5f81e3
Chris Liu <chris@ixvin.com>
2022-08-27 20:23:24 +0800

    Update code format.

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,15 +1,15 @@
-import * as path from 'path';
-import { app, BrowserWindow } from 'electron';
+import * as path from "path";
+import { app, BrowserWindow } from "electron";
 
 const loadURL = import.meta.env.PROD
-    ? `file://${path.resolve(__dirname, '../renderer/index.html')}`
-    : process['env'].RENDERER_URL || 'http://localhost:3000';
+    ? `file://${path.resolve(__dirname, "../renderer/index.html")}`
+    : process["env"].RENDERER_URL || "http://localhost:3000";
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 720,
-        icon: path.resolve(__dirname, '../../public/icons/256x256.png')
+        icon: path.resolve(__dirname, "../../public/icons/256x256.png"),
     });
 
     mainWindow.loadURL(loadURL);
@@ -20,11 +20,11 @@ function createWindow() {
 app.whenReady().then(() => {
     createWindow();
 
-    app.on('activate', function () {
+    app.on("activate", function () {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
 });
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
 });
--- src/renderer/vite.config.ts
+++ src/renderer/vite.config.ts
@@ -1,10 +1,7 @@
 import "dotenv/config";
 import { resolve } from "path";
-import { toNumber } from "lodash";
-import { defineConfig, ProxyOptions } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
-const _DEBUG = process.env.NODE_ENV !== "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
