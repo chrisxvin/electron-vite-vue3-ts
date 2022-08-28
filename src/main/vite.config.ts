c34d1f2a7c6de27045c474315af90533fe87aff4
Chris Liu <chris@ixvin.com>
2022-08-28 15:05:37 +0800

    Auto copy preload.js to dev output.

--- src/main/vite.config.ts
+++ src/main/vite.config.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import autoExternal from "rollup-plugin-auto-external";
+import rollupCopy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,12 @@ export default defineConfig({
         },
         rollupOptions: {
             plugins: [
+                rollupCopy({
+                    targets: [
+                        { src: "src/main/preload.js", dest: "dist/dev/" },
+                    ],
+                    verbose: true,
+                }),
                 autoExternal({
                     packagePath: path.resolve(__dirname, "./package.json"),
                 }),
